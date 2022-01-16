97223a0359b21a21b3e3fff745145eba93157428
Idowu Henry Ogedengbe <42342507+Hido62@users.noreply.github.com>
2022-01-16 23:41:24 +0000

    Mars Competition task 2

---
+++ NUnitTests/ManageListTest.cs
@@ -0,0 +1,66 @@
+﻿using AventStack.ExtentReports;
+using MarsCompTask2022.Utils;
+using MarsCompTask2022.Pages;
+using NUnit.Framework;
+
+
+namespace MarsCompTask2022.NUnitTests
+{
+    [TestFixture]
+    class ManageListTest : CommonDriver
+    {
+        [Test, Order(1), Description("View the Share Skill record")]
+        public void ViewManageListingsTest()
+        {
+            test = extent.CreateTest("Deleted Share Skill and Manage listing is Deleted");
+            test.Log(Status.Info, "Browser Initialisation");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(driver);
+
+            loginPageObj.LoginSteps(driver);
+
+            // Manage Listings Page object initialization and definition
+            ManageListings manageListsObj = new ManageListings(driver);
+            manageListsObj.NavigateManageListings();
+            manageListsObj.ViewManageListingsActive(driver);
+            test.Log(Status.Info, "Manage Listings of Share Skill is Deleted");
+        }
+
+        [Test, Order(2), Description("Without delete the Share Skill record")]
+        public void DeleteManageListingsTest1()
+        {
+            test = extent.CreateTest("Without delete the Share Skill and Manage listing is Deleted");
+            test.Log(Status.Info, "Browser Initialisation");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(driver);
+
+            loginPageObj.LoginSteps(driver);
+
+            // Manage Listings Page object initialization and definition
+            ManageListings manageListsObj = new ManageListings(driver);
+            manageListsObj.NavigateManageListings();
+            manageListsObj.WithoutDelManageListBtn();
+            test.Log(Status.Info, "Manage Listings of Share Skill is not Deleted");
+        }
+
+        [Test, Order(3), Description("Delete the Share Skill record")]
+        public void DeleteManageListingsTest()
+        {
+            test = extent.CreateTest("Deleted Share Skill and Manage listing is Deleted");
+            test.Log(Status.Info, "Browser Initialisation");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(driver);
+
+            loginPageObj.LoginSteps(driver);
+
+            // Manage Listings Page object initialization and definition
+            ManageListings manageListsObj = new ManageListings(driver);
+            manageListsObj.NavigateManageListings();
+            manageListsObj.DeleteManageListingBtn();
+            test.Log(Status.Info, "Manage Listings of Share Skill is Deleted");
+        }
+    }
+}
---
+++ NUnitTests/ShareSkillTest.cs
@@ -0,0 +1,154 @@
+﻿using AventStack.ExtentReports;
+using MarsCompTask2022.Pages;
+using MarsCompTask2022.Utils;
+using NUnit.Framework;
+using System;
+
+namespace MarsCompTask2022.NUnitTests
+{
+    [TestFixture]
+    class ShareSkillTest : CommonDriver
+    {
+        [Test, Order(1), Description("Create the empty Share Skill record")]
+        public void AddwithoutShareSkillTest()
+        {
+            test = extent.CreateTest("Create no Share Skill with Service Enabled");
+            test.Log(Status.Info, "Browser Initialisation");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(driver);
+
+            loginPageObj.LoginSteps(driver);
+            TestContext.WriteLine(loginPageObj);
+
+            // Home Page object initialization and definition
+            HomePage homePageObj = new HomePage();
+            homePageObj.ShareSkillBtn(driver);
+
+            //SaveScreenShotClass.SaveScreenshot1(testDriver, "TestShareSkill");
+            test.Log(Status.Info, "Share Skill Page is Opened");
+
+            // Share Skill Page object initialization and definition
+            ShareSkill shareSkillObj = new ShareSkill(driver);
+
+            shareSkillObj.AddwithoutData(driver);
+            shareSkillObj.SaveShareSkill();
+            test.Log(Status.Info, "ShareSkill is not Saved");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(35);
+        }
+
+        [Test, Order(2), Description("Create the invalid Share Skill record")]
+        public void AddInvaildShareSkillTest()
+        {
+            test = extent.CreateTest("Create invalid Share Skill with Service Enabled");
+            test.Log(Status.Info, "Browser Initialisation");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(driver);
+
+            loginPageObj.LoginSteps(driver);
+            TestContext.WriteLine(loginPageObj);
+
+            // Home Page object initialization and definition
+            HomePage homePageObj = new HomePage();
+            homePageObj.ShareSkillBtn(driver);
+
+            //SaveScreenShotClass.SaveScreenshot1(testDriver, "TestShareSkill");
+            test.Log(Status.Info, "Share Skill Page is Opened");
+
+            // Share Skill Page object initialization and definition
+            ShareSkill shareSkillObj = new ShareSkill(driver);
+
+            shareSkillObj.AddInvaildTitle(driver);
+            shareSkillObj.AddInvaildDescription(driver);
+            shareSkillObj.AddInvalidEnterTags(driver);
+            shareSkillObj.ServiceTypeHourly();
+            shareSkillObj.LocationTypeOnline();
+            shareSkillObj.AddInvalidAvailDays(driver);
+            shareSkillObj.AddInvalidSkillExchange(driver);
+            shareSkillObj.ActiveShareSkill();
+            shareSkillObj.SaveShareSkill();
+            test.Log(Status.Info, "ShareSkill is not Saved");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(35);
+        }
+
+        [Test, Order(3), Description("Create the valid Share Skill record")]
+        public void CreateShareSkillTest()
+        {
+            test = extent.CreateTest("Create Share Skill with Service Enabled");
+            test.Log(Status.Info, "Browser Initialisation");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(driver);
+
+            loginPageObj.LoginSteps(driver);
+            TestContext.WriteLine(loginPageObj);
+
+            // Home Page object initialization and definition
+            HomePage homePageObj = new HomePage();
+            homePageObj.ShareSkillBtn(driver);
+
+            //SaveScreenShotClass.SaveScreenshot1(testDriver, "TestShareSkill");
+            test.Log(Status.Info, "Share Skill Page is Opened");
+
+            // Share Skill Page object initialization and definition
+            ShareSkill shareSkillObj = new ShareSkill(driver);
+
+            shareSkillObj.AddTitle(driver);
+            shareSkillObj.AddDescription(driver);
+            shareSkillObj.SelectAddCategory(driver);
+            shareSkillObj.SelectAddSubCategory(driver);
+            shareSkillObj.AddEnterTags(driver);
+            shareSkillObj.ServiceTypeHourly();
+            shareSkillObj.LocationTypeOnline();
+            shareSkillObj.AddAvailableDays(driver);
+            shareSkillObj.SkillExchange(driver);
+            shareSkillObj.ActiveShareSkill();
+            shareSkillObj.SaveShareSkill();
+            test.Log(Status.Info, "ShareSkill is Saved");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(35);
+
+            // Manage Listings Page object initialization and definition
+            ManageListings manageListsObj = new ManageListings(driver);
+            manageListsObj.AddManageListingsActive(driver);
+            test.Log(Status.Pass, "Assert Pass as condition is True & Manage listing is active");
+        }
+
+        [Test, Order(4), Description("Edit the valid Share Skill record")]
+        public void EditShareSkillTest()
+        {
+            test = extent.CreateTest("Edit Share Skill with Skill Trade as Skill Exchange and change it to Credit");
+            test.Log(Status.Info, "Browser Initialisation");
+
+            // Login Page object initialization and definition
+            LoginPage loginPageObj = new LoginPage(driver);
+            loginPageObj.LoginSteps(driver);
+
+            // Manage Listings Page object initialization and definition
+            ManageListings manageListsObj = new ManageListings(driver);
+            manageListsObj.NavigateManageListings();
+            manageListsObj.EditManageListings();
+
+            // Share Skill Page object initialization and definition
+            ShareSkill shareSkillObj = new ShareSkill(driver);
+
+            test.Log(Status.Info, "Share Skill Page is Opened");
+
+            shareSkillObj.EditTitle(driver);
+            shareSkillObj.EditDescription(driver);
+            shareSkillObj.SelectEditCategory(driver);
+            shareSkillObj.SelectEditSubCategory(driver);
+            shareSkillObj.EditTags(driver);
+            shareSkillObj.ServiceTypeOne_off();
+            shareSkillObj.LocationTypeOnsite();
+            shareSkillObj.EditAvailableDays(driver);
+            shareSkillObj.EditCreditExchange(driver);
+            shareSkillObj.HiddenShareSkill();
+            shareSkillObj.SaveShareSkill();
+            test.Log(Status.Info, "ShareSkill is Saved");
+
+            manageListsObj.EditManageListingsHidden(driver);
+            test.Log(Status.Pass, "Assert Pass as condition is True and Manage listing is Hidden");
+        }
+    }
+}
